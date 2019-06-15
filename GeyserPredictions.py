74f543ea18a686bacaf0355b7e0e5340c5cc2708
Matthew Kesselring <mskessel@ncsu.edu>
2019-06-15 12:58:12 -0400

    Attribution text wraps

--- GeyserPredictions.py
+++ GeyserPredictions.py
@@ -5,16 +5,18 @@ from tkinter import *
 import tkinter.ttk
 
 url = {
-    "base": "https://www.geysertimes.org/api/v5",
-    "geysers": "https://www.geysertimes.org/api/v5/geysers",
+    "base"       : "https://www.geysertimes.org/api/v5",
+    "geysers"    : "https://www.geysertimes.org/api/v5/geysers",
     "predictions": "https://www.geysertimes.org/api/v5/predictions_latest"
 }
 format_string = "%h %d %I:%M %p"
-update_format  = "%h %d %I:%M:%S %p"
+update_format = "%h %d %I:%M:%S %p"
 center_format = None
-attribution = "Contains information from GeyserTimes, which is made available here under the Open Database"
-attribution2 = "License (ODbL). https://Geysertimes.org https://opendatacommons.org/licenses/odbl/"
-freq = int(1000*60*5)  # ms/s * s/min * min
+attribution = "Contains information from GeyserTimes, which is made "\
+              "available here under the Open Database License (ODbL). "\
+              "https://Geysertimes.org "\
+              "https://opendatacommons.org/licenses/odbl/"
+freq = int(1000 * 60 * 5)  # ms/s * s/min * min
 root = None
 geyser_labels = []
 open_labels = []
@@ -23,7 +25,7 @@ close_labels = []
 probability_labels = []
 titles = []
 updated_label = None
-row_offset = 4
+row_offset = 3
 cols = None
 attributeFont = ("Calibri", "10")
 boldFont = ("Calibri", "12", "bold")
@@ -31,14 +33,16 @@ myFont = ("Calibri", "12")
 
 
 def update():
-    global geyser_labels, open_labels, close_labels, probability_labels, updated_label
+    global geyser_labels, open_labels, close_labels, probability_labels,\
+        updated_label
     # global center_labels
 
     # Print time to console
     print(datetime.datetime.now())
-    last_update = "Last Update: " + datetime.datetime.now().strftime(update_format)
+    last_update = "Last Update: " + datetime.datetime.now().strftime(
+            update_format)
     updated_label = Label(root, text=last_update, font=myFont)
-    updated_label.grid(row=2, column=0, columnspan=cols)
+    updated_label.grid(row=row_offset-2, column=0, columnspan=cols)
 
     # Get available predictions
     r = requests.get(url["predictions"])
@@ -78,12 +82,20 @@ def update():
         probability = p["probability"]
 
         geyser_labels.append(Label(root, text=name, font=myFont))
-        open_labels.append(Label(root, text=window_open.strftime(format_string), font=myFont))
-        # center_labels.append(Label(root, text=center.strftime(format_string), font=myFont))
-        close_labels.append(Label(root, text=window_close.strftime(format_string), font=myFont))
+        open_labels.append(
+                Label(root, text=window_open.strftime(format_string),
+                      font=myFont))
+        # center_labels.append(Label(root, text=center.strftime(
+        # format_string), font=myFont))
+        close_labels.append(
+                Label(root, text=window_close.strftime(format_string),
+                      font=myFont))
         probability_labels.append(Label(root, text=probability, font=myFont))
 
-        tkinter.ttk.Separator(root, orient=HORIZONTAL).grid(column=0, row=row_num, columnspan=cols, sticky="ew")
+        tkinter.ttk.Separator(root, orient=HORIZONTAL).grid(column=0,
+                                                            row=row_num,
+                                                            columnspan=cols,
+                                                            sticky="ew")
         row_num += 1
         geyser_labels[i].grid(row=row_num, column=0)
         open_labels[i].grid(row=row_num, column=1)
@@ -115,14 +127,16 @@ def main():
 
     global cols
     cols = len(titles)
-
-    attribute_label = Label(root, text=attribution, font=attributeFont)
-    attribute_label.grid(row=0, column=0, columnspan=cols)
-    attribute2_label = Label(root, text=attribution2, font=attributeFont)
-    attribute2_label.grid(row=1, column=0, columnspan=cols)
-
     for i in range(0, len(titles)):
-        titles[i].grid(row=3, column=i)
+        t_width = titles[i].winfo_reqwidth()
+        titles[i].grid(row=row_offset - 1, column=i, padx=t_width*0.1)
+    root.update()
+    width = root.winfo_width()
+
+    attribute_label = Label(root, text=attribution, font=attributeFont,
+                            wraplength=width, justify=CENTER)
+    attribute_label.grid(row=0, column=0, columnspan=cols)
+
     root.after(0, update)
     root.mainloop()
 
